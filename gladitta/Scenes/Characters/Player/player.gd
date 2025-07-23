036adb4f9e15b5a2e8fd2efe78ea42bc5677bfb0
BrunoBandeiraSilva <bcbs7980@gmail.com>
2025-07-23 00:25:59 -0300

    Implemented Sword Behaviour

--- gladitta/Scenes/Characters/Player/player.gd
+++ gladitta/Scenes/Characters/Player/player.gd
@@ -5,46 +5,82 @@ extends CharacterBody2D
 @export var SPEED = 75.0
 @export var JUMP_VELOCITY = -200.0
 
-var sword_scene = load("res://Scenes/Characters/Sword/sword.tscn")
+var sword_scene = load("res://Scenes/Characters/Player/Sword/sword.tscn")
+
+var orientation = 1
+
+func _ready() -> void:
+	$Sword/Sprite2D.hide()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		#velocity.y = clamp(velocity.y, -INF, TERMINAL_FALLING_VELOCITY)
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_released("jump"):
 		velocity.y *= 0.5
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("move_right"):
+		velocity.x = SPEED
+		orientation = 1
+		move_sword("right")
 		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -SPEED
+		orientation = -1
+		move_sword("left")
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("move_down"):
+		velocity.x = 0.0
+		move_sword("down")
+	elif Input.is_action_pressed("move_up"):
+		velocity.x = 0.0
+		move_sword("up")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0.0
+		if orientation == 1:
+			move_sword("right")
+		else:
+			move_sword("left")
 		$AnimatedSprite2D.play("idle")
 
-	if direction == 1:
-		$AnimatedSprite2D.flip_h = false
-	elif direction == -1:
-		$AnimatedSprite2D.flip_h = true
+	if Input.is_action_just_pressed("attack"):
+		$Sword/Sprite2D.show()
+		await get_tree().create_timer(0.1).timeout
+		$Sword/Sprite2D.hide()
 
 	if not is_on_floor():
 		$AnimatedSprite2D.play("jump")
 
-	if Input.is_action_just_pressed("attack"):
-		var sword_instance = sword_scene.instantiate()
-		get_parent().add_child(sword_instance)
-		sword_instance.set_direction(direction)
-		sword_instance.global_position.x = self.global_position.x
-		sword_instance.global_position.y = self.global_position.y + 5
-
 	move_and_slide()
 
-func launch():
-	pass
+func move_sword(direction):
+	match direction:
+		"right":
+			$Sword.position = Vector2(8, 0)
+			$Sword/Sprite2D.flip_h = false
+			$Sword/Sprite2D.rotation_degrees = 0
+		"left":
+			$Sword.position = Vector2(-8, 0)
+			$Sword/Sprite2D.flip_h = true
+			$Sword/Sprite2D.rotation_degrees = 0
+		"down":
+			$Sword.position = Vector2(0, 8)
+			$Sword/Sprite2D.flip_h = false
+			$Sword/Sprite2D.rotation_degrees = 90
+		"up":
+			$Sword.position = Vector2(0, -8)
+			$Sword/Sprite2D.flip_h = false
+			$Sword/Sprite2D.rotation_degrees = -90
+
+func launch(direction):
+	print("chamou")
+	velocity.y = direction * 100
+ 
+func _on_sword_body_entered(body: Node2D) -> void:
+	if $Sword/Sprite2D.rotation_degrees == -90:
+		launch(Vector2(0, -1))
