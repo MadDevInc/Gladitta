ec8bd369a43cd9723a22940e96723383b8e20cc6
BrunoBandeiraSilva <bcbs7980@gmail.com>
2025-08-04 18:43:26 -0300

    Changed Level Ending Sprite

--- gladitta/Scenes/Gadgets/Spring/spring.gd
+++ gladitta/Scenes/Gadgets/Spring/spring.gd
@@ -8,9 +8,9 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") or body.is_in_group("Enemy"):
 		body.launch(-direction)
 	if body.is_in_group("Arrow"):
-		body.queue_free()
+		body.get_parent().queue_free()
 		var arrow_instance = arrow_scene.instantiate()
-		get_parent().get_node("Arrows").add_child(arrow_instance)
+		get_parent().get_node("Arrows").call_deferred("add_child", arrow_instance)
 		arrow_instance.global_position = self.global_position
 		arrow_instance.set_direction(direction)
 	$AnimatedSprite2D.play("expand")
