be39be1a46e5f7462b99dfb40997f8e84b9243b6
BrunoBandeiraSilva <bcbs7980@gmail.com>
2025-09-15 11:20:20 -0300

    Fixed Stage Icon Display

--- gladitta/Scenes/Levels/LevelSelection/StageIcon/stage_icon.gd
+++ gladitta/Scenes/Levels/LevelSelection/StageIcon/stage_icon.gd
@@ -3,14 +3,12 @@ extends Control
 func hover():
 	$AnimatedSprite2D.play("hover")
 	$AnimationPlayer.play("hover")
-	$LevelRender.show()
-	$Overlay.show()
+	$Display.show()
 
 func unhover():
 	if $AnimatedSprite2D.animation == "hover":
 		$AnimatedSprite2D.play("unhover")
-		$LevelRender.hide()
-		$Overlay.hide()
+		$Display.hide()
 
 func display(scene):
 	$SubViewportContainer/SubViewport.add_child(scene)
